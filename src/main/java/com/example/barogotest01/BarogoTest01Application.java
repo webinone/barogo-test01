ed00f0bd59923ed2958e1d2491e4bdf65c2a2a34
Youngjo Jang <youngjo.jang@gmail.com>
2020-10-27 18:30:18 +0900

    최초 수정

--- src/main/java/com/example/barogotest01/BarogoTest01Application.java
+++ src/main/java/com/example/barogotest01/BarogoTest01Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BarogoTest01Application {
-
+    // 수정 !!!
     public static void main(String[] args) {
         SpringApplication.run(BarogoTest01Application.class, args);
     }
