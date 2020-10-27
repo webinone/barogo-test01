fc2ca82ca90b472920701aca71817c8879a9aaf6
Youngjo Jang <youngjo.jang@gmail.com>
2020-10-27 18:57:49 +0900

    병합 준비 최종 !!!!

--- src/main/java/com/example/barogotest01/BarogoTest01Application.java
+++ src/main/java/com/example/barogotest01/BarogoTest01Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BarogoTest01Application {
     // 수정 !!! 2222223333344444
     // 수정이 새로 생기따 !!!!
+    // 수정 ㅇㅇㅇㅇㅇ
     public static void main(String[] args) {
         SpringApplication.run(BarogoTest01Application.class, args);
     }
