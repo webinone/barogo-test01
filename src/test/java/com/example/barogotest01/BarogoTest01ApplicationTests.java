010ac2be20008d6659828f20dfd9833b887f57cc
Youngjo Jang <youngjo.jang@gmail.com>
2020-10-27 18:18:38 +0900

    init

---
+++ src/test/java/com/example/barogotest01/BarogoTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barogotest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarogoTest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
