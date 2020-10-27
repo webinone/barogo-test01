a6d2b70bc33008bfd491cbb7edd480cb3e964ebd
Youngjo Jang <youngjo.jang@gmail.com>
2020-10-27 18:37:12 +0900

    modify 4444

--- src/main/java/com/example/barogotest01/BarogoTest01Application.java
+++ src/main/java/com/example/barogotest01/BarogoTest01Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BarogoTest01Application {
-    // 수정 !!! 22222233333
+    // 수정 !!! 2222223333344444
     public static void main(String[] args) {
         SpringApplication.run(BarogoTest01Application.class, args);
     }
